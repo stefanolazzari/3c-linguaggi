12561353f70f505d3b163b57dc56db68057792e1
stefanolazzari <stefanolazzari79@libero.it>
2025-01-18 16:01:01 +0000

    aggiornamento

---
+++ funzioni.js
@@ -0,0 +1,101 @@
+
+
+
+//dichiarazione di funzione
+function saluto(){
+    alert("Ciao 3c");
+}
+
+
+saluto(); //chiamata alla funzione
+
+function somma(a,b){
+    return a + b;
+}
+
+function moltiplizione(a,b){
+    return a * b;
+}
+
+function calcolo(a,b,operazione){
+
+    return operazione(a,b);
+
+}
+
+
+//dichiarazione di funzione
+function saluto(){
+    console.log("ciao");
+}
+//chiamata alla funzione
+saluto(); //chiamata alla funzione
+saluto(); //
+saluto();
+saluto();
+
+
+
+
+
+
+
+
+
+
+
+console.log(calcolo(3,6,somma));
+
+console.log(calcolo(3,6,moltiplizione));
+
+
+
+
+
+
+
+
+// GIT    
+// CONTROLLO DI VERSIONE
+
+// GITHUB  
+// CONTROLLO DI VERSIONE REMOTA
+
+
+// Javascript
+
+// Dichiarazione di una variabile:
+// var : obsoleto, visibilità globale meno sicura
+// let : visibilità di blocco (scope) 
+// const : visibilità di blocco (scope), dichiara variabili che non possono essere riassegnate
+
+  
+
+
+ {
+    let miaVariabile = 68; 
+ }
+
+ {
+    var miaVariabile = 90;
+ }
+
+console.log(miaVariabile); //90
+
+//wIUEFGLIWEUFGWIEUFGòWQIEUFWòIUF
+{
+const costante1 = 9.18;
+}
+
+console.log(costante1); //9.18
+
+costante1 = 80; //errore 
+
+console.log(costante1); //errore
+
+
+
+
+
+
+
---
+++ variabili.js
@@ -0,0 +1,43 @@
+
+
+/* console.log("qui nomeVariale1 é: " + nomeVariabile1);
+
+nomeVariabile1 = "Ciao";
+
+console.log("qui nomeVariale1 é: " + nomeVariabile1);
+
+
+{    
+    var nomeVariabileConVar = 567; //obsoleto
+    let nomeVariabileConLet = 43;
+    console.log(nomeVariabileConLet);
+} */
+
+{
+    var a = 23;
+}
+
+
+{
+    let a = "Ciao";
+    console.log(a);
+}
+
+console.log(a); //???
+
+
+const pigreco = 3.14;
+
+console.log("pi: " + pigreco);
+
+pigreco = 3.15;
+
+
+
+
+
+
+
+
+
+
